8c7b7bf696afa217609d5ce981248cf3ef82445b
Maximilian Nöthe <maximilian.noethe@tu-dortmund.de>
2022-06-25 12:37:00 +0200

    Add project structure using setuptools_scm

---
+++ src/project_template_python_pure/__init__.py
@@ -0,0 +1,8 @@
+"""
+Template pure python projects.
+"""
+from .version import __version__
+
+__all__ = [
+    "__version__",
+]
---
+++ src/project_template_python_pure/tests/test_version.py
@@ -0,0 +1,3 @@
+def test_version():
+    from project_template_python_pure import __version__
+    assert __version__ != '0.0.0'
