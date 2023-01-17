801594eeb36072284ac9c5ceb7320aa3c72a4def
maxnoe <maximilian.linhoff@tu-dortmund.de>
2023-01-17 10:52:46 +0000

    Deploying to gh-pages from @ cta-observatory/project-template-python-pure@9bf2c2c2d6f023f95dcb6e65b727e01d63d34b02 🚀

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.2.1.dev4+g5fed7dc',
+    VERSION: '0.2.1.dev5+g9bf2c2c',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
